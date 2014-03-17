8fb5c01e814649b5ba15613c3313409233286ed3
007ace <ayushkapur@gmail.com>
2014-03-17 19:06:42 -0400

    Motor Pot

--- competition_code/competition_code.ino
+++ competition_code/competition_code.ino
@@ -21,7 +21,7 @@ static Servo brushless;
 static Rudder rudder;
 static Servo rudder_servo;
 
-int state=LOW, reading, previous=LOW, pot_power;
+int state=LOW, reading, previous=LOW, pot_power, pot_power_range;
 double reference, input, output;
 long time=0;
 
@@ -31,7 +31,6 @@ void setup (){
   pinMode(PIN_MAIN_SWITCH, INPUT);
   pinMode(PIN_SPEED_POT, INPUT);
   Serial.begin (9600);
-  pot_power = analogRead(PIN_SPEED_POT);
   init_sensor (&side, PIN_SIDE_SENSOR);
   init_sensor (&angled, PIN_ANGLED_SENSOR);
   arm_motor(&brushless, PIN_MOTOR_CONTROL);
@@ -56,24 +55,16 @@ void loop (){
     else
     {
       state = HIGH;
-      motor_speed(&brushless, pot_power);
+      pot_power = analogRead(PIN_SPEED_POT);
+      pot_power_range = map(pot_power,0,1023,0,100);
+      motor_speed(&brushless, pot_power_range);
     }
     time=millis();
   }
-  previous = reading;
   float side_sensor = read_distance (&side);
   float angle_sensor = read_distance (&angled);
-  Serial.print ("{");
-  Serial.print (side_sensor);
-  Serial.print (",");
-  Serial.print (angle_sensor);
-  Serial.print ("}");
-  Serial.print ("->");
-  
   input = side_sensor;
   rudder_control.Compute ();  
   set_angle (&rudder, output);
-  
-  Serial.println (output);
   delay (40);
 }
--- competition_code/motor_control.cpp
+++ competition_code/motor_control.cpp
@@ -7,23 +7,19 @@ void arm_motor(Servo * brushless, int pin){
   brushless->attach(pin);
   brushless->write(100);
   delay(10000);
-  //brushless.write();
   
 }
 void motor_speed(Servo * brushless, int power){
   int throttle = 0;
-  //arm_motor();
   if(power<0 || power>100)
-  Serial.println("% power not in range");
-  else
+  return;
   {
-      throttle = map(power, 0, 100, 0, 179);
+      throttle = map(power, 0, 100, 100, 179);
       brushless->write(throttle);
-      Serial.println(throttle);
-}
+  }
 }
 void stop_motor(Servo * brushless){
-  int stop_val = map(100, 0, 100, 0, 179);
+  int stop_val = map(56, 0, 100, 0, 179);
   brushless->write(stop_val);
 }
 
